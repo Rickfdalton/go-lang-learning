8b0303d765121b2c974e6e6994486916973e151a
jathavan mahendrarajah <physickness@gmail.com>
2026-05-27 17:32:45 +0530

    added connection timeout

--- tcp-client.go
+++ tcp-client.go
@@ -5,20 +5,15 @@ import (
 	"net"
 	"io"
 	"bufio"	
+	"time"
 )
 
-func main(){
-
-	conn, err := net.Dial("tcp", "localhost:9090")
-	if err!= nil {
-		fmt.Println("cannot dial server", err)
-		return
-	}
-	defer conn.Close()
-	_,err = conn.Write([]byte("Hi from client\n"))
+func sendMessage(conn net.Conn,msg string ) (string, error) {	
+	_,err := conn.Write([]byte(msg+"\n"))
 	if err!= nil{
 			fmt.Println("Failed to write the data ",err )
-			return
+			return "",err
+
 	}
 	reader := bufio.NewReader(conn)
 	
@@ -26,11 +21,38 @@ func main(){
 	if err!= nil{
 		if err!= io.EOF {
 			fmt.Println("Error reading data: ", err)
-			return
 		}
-		return
+			return "",err
 	}
 	line:= "From Backend: "+string(bytes)
-	fmt.Println(line)
+	return line, nil
+}
 
+func main(){
+
+	conn, err := net.Dial("tcp", "localhost:9090")
+	if err!= nil {
+		fmt.Println("cannot dial server", err)
+			return 
+	}
+	defer conn.Close()
+
+	for {
+		fmt.Print("Enter the message to send: ") 
+		var msg string
+		_,err := fmt.Scanf("%s", &msg)
+		if err!=nil{
+			fmt.Print("sorry cannot read your message")
+			continue
+		}
+		response, err :=sendMessage(conn,msg)
+		if err!= nil {
+			fmt.Println("Connection expired")
+		}else{
+			fmt.Println(response)
+		}
+		conn.SetDeadline(time.Now().Add(5 * time.Second))
+	
+	}
+	
 }
